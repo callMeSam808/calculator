ce5d4361facd3b74cad8f670b9ef16c09ee1c4cd
Oshamaru <callMeSam808@gmail.com>
2022-03-28 16:10:25 -1000

    Create operate function

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,20 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+function operate (operator, a, b) {
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+    }
+    return result;
+}
